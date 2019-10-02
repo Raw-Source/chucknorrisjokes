99b320e21c8e8fc52f0f4abf4a10a81379044f6e
Raw-Source <38597348+Raw-Source@users.noreply.github.com>
2019-10-02 15:26:02 +0200

    initial Chuck Norris Jokes

---
+++ ChuckNorrisJokes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ChuckNorrisJokesLib;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ChuckNorrisJokes
+{
+    class Program   
+    {
+        static async Task Main(string[] args)
+        {
+           
+            JokesGenerator jokesGenerator = new JokesGenerator();
+            string randomJoke = await jokesGenerator.GetRandomJoke();
+
+            Console.WriteLine(randomJoke);
+
+            string[] categories = await jokesGenerator.GetCategories();
+
+            for(int index = categories.Length - 1; index >= 0; index --)
+            {
+                Console.WriteLine(categories[index]);
+            }
+        } 
+    }
+}
